e8581cdfcf9fd256cd71d24433a4a7b0391129ab
Mert Gonkesen <mertgnksn@gmail.com>
2022-06-17 12:59:02 +0200

    open gl draw triangle

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/fixmenu/gogldemo
 
 go 1.18
+
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/veandco/go-sdl2 v0.4.21 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,152 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+	"unsafe"
 
+	"github.com/go-gl/gl/v3.2-core/gl"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	minWidth  = 1280
+	minHeight = 720
+)
+
+func main() {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		panic(err)
+	}
+
+	sdl.GLSetAttribute(sdl.GL_CONTEXT_MAJOR_VERSION, 3)
+	sdl.GLSetAttribute(sdl.GL_CONTEXT_MINOR_VERSION, 3)
+	sdl.GLSetAttribute(sdl.GL_CONTEXT_PROFILE_MASK, sdl.GL_CONTEXT_PROFILE_CORE)
+
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Triangle", 200, 200, minWidth, minHeight, sdl.WINDOW_OPENGL)
+
+	if err != nil {
+		panic(err)
+	}
+	window.GLCreateContext()
+	defer window.Destroy()
+
+	gl.Init()
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+
+	fmt.Println(version)
+
+	vertexShaderSource := `
+		#version 330 core
+		layout (location = 0) in vec3 aPos;
+
+		void main() {
+			gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+		}
+	`
+
+	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
+
+	cSource, free := gl.Strs(vertexShaderSource)
+	gl.ShaderSource(vertexShader, 1, cSource, nil)
+	free()
+	gl.CompileShader(vertexShader)
+
+	var status int32
+
+	gl.GetShaderiv(vertexShader, gl.COMPILE_STATUS, &status)
+
+	if status == gl.FALSE {
+		var logLength int32
+
+		gl.GetShaderiv(vertexShader, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(vertexShader, logLength, nil, gl.Str(log))
+		panic("failed: \n" + log)
+	}
+
+	fragmentShaderSource := `
+		#version 330 core
+		out vec4 FragColor;
+		
+		void main()
+		{
+			FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);
+		} 
+	`
+
+	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	cSource, free = gl.Strs(fragmentShaderSource)
+	gl.ShaderSource(fragmentShader, 1, cSource, nil)
+	free()
+	gl.CompileShader(fragmentShader)
+
+	gl.GetShaderiv(fragmentShader, gl.COMPILE_STATUS, &status)
+
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(fragmentShader, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(fragmentShader, logLength, nil, gl.Str(log))
+		panic("failed: \n" + log)
+	}
+
+	shaderProgram := gl.CreateProgram()
+	gl.AttachShader(shaderProgram, vertexShader)
+	gl.AttachShader(shaderProgram, fragmentShader)
+	gl.LinkProgram(shaderProgram)
+
+	var success int32
+	gl.GetProgramiv(shaderProgram, gl.LINK_STATUS, &success)
+
+	if success == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shaderProgram, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shaderProgram, logLength, nil, gl.Str(log))
+		panic("failed: \n" + log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	vertices := []float32{
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0,
+	}
+
+	var VBO uint32
+	gl.GenBuffers(1, &VBO)
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+
+	var VAO uint32
+	gl.GenVertexArrays(1, &VAO)
+	gl.BindVertexArray(VAO)
+	gl.BufferData(gl.ARRAY_BUFFER, int(unsafe.Sizeof(vertices)), gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, nil)
+	gl.EnableVertexAttribArray(0)
+	gl.BindVertexArray(0)
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		gl.ClearColor(0.0, 0.0, 0.0, 0.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		gl.UseProgram(shaderProgram)
+		gl.BindVertexArray(VAO)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		window.GLSwap()
+	}
 }
